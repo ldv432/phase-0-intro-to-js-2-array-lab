eedb0eacc4949be9b3a4047cb141c8df300e371f
Luke <ldv432@gmail.com>
2023-09-26 16:04:24 -0600

    used array methods to pass tests

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    const appendCat = [...cats, "Broom"];
+    return appendCat
+}
+
+function prependCat(){
+    const prependCat = ["Arnold", ...cats];
+    return prependCat
+}
+
+function removeLastCat(){
+    const removeLastCat = cats.slice(0, 2);
+    return removeLastCat
+}
+
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(1, 3);
+    return removeFirstCat
+}
